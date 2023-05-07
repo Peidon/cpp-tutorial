8f1f419184e3b871faf3166a462e856e4fc8cb0c
peidong.xu <peidong.xu@shopee.com>
2023-05-07 23:02:29 +0800

    use c++ 11

--- main.cpp
+++ main.cpp
@@ -7,22 +7,26 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    // vector<int> a{1,2,3};
-    // vector<int> b{4,5,6};
+    vector<int> second = { 10, 20, 30, 30, 20, 10, 10, 20 };
+    vector<int> a = {1, 2, 3};
+    vector<int> b = {4, 5, 6};
 
-    // ListNode* l1 = buildList(a);
-    // ListNode* l2 = buildList(b);
+    ListNode* l1 = buildList(a);
+    ListNode* l2 = buildList(b);
 
-    // Solution* so = new Solution();
-    // ListNode* l = so->addTwoNumbers(l1, l2);
+    Solution* so = new Solution();
+    ListNode* l = so->addTwoNumbers(l1, l2);
 
-    // while (l)
-    // {
-    //     cout << l->val << " ";
-    //     l = l->next;
-    // }
+    while (l)
+    {
+        cout << l->val << " ";
+        l = l->next;
+    }
 
-    if (argc < 2) {
+    cout << endl;
+
+    if (argc < 2)
+    {
         // report version
         std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
