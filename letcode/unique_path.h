5871c789aab24f75aa3007a7de78c75a5cc0c71f
peidong.xu <peidong.xu@shopee.com>
2023-06-07 09:59:41 +0800

    unique paths optimize

--- letcode/unique_path.h
+++ letcode/unique_path.h
@@ -8,6 +8,31 @@ namespace letcode63
     class Solution
     {
     public:
+        int uniquePathsWithObstaclesOpti(vector<vector<int>> &obstacleGrid)
+        {
+            int n = obstacleGrid.size(), m = obstacleGrid.at(0).size();
+            vector<int> f(m);
+
+            f[0] = (obstacleGrid[0][0] == 0);
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    if (obstacleGrid[i][j] == 1)
+                    {
+                        f[j] = 0;
+                        continue;
+                    }
+                    if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0)
+                    {
+                        f[j] += f[j - 1];
+                    }
+                }
+            }
+
+            return f.back();
+        }
+
         int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
         {
             vector<vector<int>> dp(obstacleGrid.begin(), obstacleGrid.end());
@@ -22,7 +47,7 @@ namespace letcode63
 
                 if (v.size() > 0 && i > 0)
                 {
-                    dp[i][0] = dp[i-1][0];
+                    dp[i][0] = dp[i - 1][0];
                     if (obstacleGrid[i][0] == 1)
                     {
                         dp[i][0] = 0;
@@ -33,7 +58,7 @@ namespace letcode63
                 {
                     if (i == 0)
                     {
-                        dp[i][j] = dp[i][j-1];
+                        dp[i][j] = dp[i][j - 1];
                     }
                     if (obstacleGrid[i][j] == 1)
                     {
@@ -43,7 +68,7 @@ namespace letcode63
 
                     if (i > 0)
                     {
-                        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                     }
                 }
             }
